8039cc9988609afa0e4be6c01bb2c2e4828cf378
Tohir <towuda564@gmail.com>
2025-10-18 11:31:01 +0100

    Progress save

--- app/src/main/java/com/tohir/booksandstuff/ui/books/EpubReaderFragment.kt
+++ app/src/main/java/com/tohir/booksandstuff/ui/books/EpubReaderFragment.kt
@@ -5,7 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.net.toUri
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commitNow
 import androidx.fragment.app.viewModels
@@ -17,37 +20,47 @@ import com.tohir.booksandstuff.databinding.FragmentReaderBinding
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import org.readium.r2.navigator.VisualNavigator
 import org.readium.r2.navigator.epub.EpubNavigatorFactory
 import org.readium.r2.navigator.epub.EpubNavigatorFragment
+import org.readium.r2.navigator.input.InputListener
+import org.readium.r2.navigator.input.TapEvent
 import java.io.File
 
 class EpubReaderFragment : Fragment() {
 
     private val viewModel: ReaderViewModel by viewModels()
-    lateinit var navigator: EpubNavigatorFragment
     private lateinit var binding: FragmentReaderBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentReaderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-
-        super.onCreate(savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val bookPath = arguments?.getString("BOOK_PATH")
         val bookUri = arguments?.getString("BOOK_URI")
         val bookID = arguments?.getInt("BOOK_ID")
         Log.d("tohir", "EpubReaderFragment, Gotten $bookUri")
 
-        if (bookPath != null) {
+        val bookFile = bookPath?.let { File(it) } ?: bookUri?.let { File(it) }
+
+        if (bookFile != null && bookID != null) {
             lifecycleScope.launch {
                 val publication =
-                    viewModel.importPublication(bookPath.toUri(), requireContext(), bookID)
+                    viewModel.importPublication(bookFile.toUri(), requireContext(), bookID)
 
                 val navigatorFactory = EpubNavigatorFactory(publication = publication)
 
                 childFragmentManager.fragmentFactory =
                     navigatorFactory.createFragmentFactory(
-                        viewModel.restoreReadingProgression(bookID!!)
+                        viewModel.restoreReadingProgression(bookID)
                     )
 
                 val tag = "EpubNavigatorFragment"
@@ -62,63 +75,50 @@ class EpubReaderFragment : Fragment() {
                     }
                 }
 
-                navigator = childFragmentManager.findFragmentByTag(tag) as EpubNavigatorFragment
+                setupClickListeners()
+
+                val navigator = childFragmentManager.findFragmentByTag(tag) as EpubNavigatorFragment
+
+                (navigator as VisualNavigator).apply {
+                    addInputListener(object : InputListener {
+                        override fun onTap(event: TapEvent): Boolean {
+
+                            if (binding.imageViewCancelButton.isVisible) {
+                                binding.imageViewCancelButton.visibility = View.INVISIBLE
+                            } else if (binding.imageViewCancelButton.isInvisible) {
+                                binding.imageViewCancelButton.visibility = View.VISIBLE
+                            }
+
+                            if (binding.imageViewOptions.isVisible) {
+                                binding.imageViewOptions.visibility = View.INVISIBLE
+                            } else if (binding.imageViewOptions.isInvisible) {
+                                binding.imageViewOptions.visibility = View.VISIBLE
+                            }
+
+
+
+                            return true
+                        }
+                    })
+                }
 
                 viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                     navigator.currentLocator
                         .onEach { viewModel.saveReadingProgression(it, bookID) }
                         .launchIn(this)
                 }
-
-            }
-        } else {
-            lifecycleScope.launch {
-                val publication =
-                    viewModel.importPublication(File(bookUri!!).toUri(), requireContext(), bookID)
-
-                val navigatorFactory = EpubNavigatorFactory(publication = publication)
-
-                childFragmentManager.fragmentFactory =
-                    navigatorFactory.createFragmentFactory(
-                        viewModel.restoreReadingProgression(
-                            bookID!!
-                        )
-                    )
-
-                val tag = "EpubNavigatorFragment"
-                if (savedInstanceState == null) {
-                    childFragmentManager.commitNow {
-                        add(
-                            R.id.fragment_reader_container,
-                            EpubNavigatorFragment::class.java,
-                            Bundle(),
-                            tag
-                        )
-                    }
-                }
-
-                navigator = childFragmentManager.findFragmentByTag(tag) as EpubNavigatorFragment
-
-                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    navigator.currentLocator
-                        .onEach { viewModel.saveReadingProgression(it, bookID) }
-                        .launchIn(this)
-                }
-
             }
         }
     }
 
+    fun setupClickListeners() {
+        binding.imageViewCancelButton.setOnClickListener {
+            requireActivity().finish()
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        binding.imageViewOptions.setOnClickListener {
 
-    ): View {
-        binding = FragmentReaderBinding.inflate(inflater, container, false)
-        return binding.root
+        }
     }
-
 
 }
